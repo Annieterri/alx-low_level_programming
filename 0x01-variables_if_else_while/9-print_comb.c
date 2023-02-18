1f14a50d194cd08a417c8eed9d4b1e8781c06e76
Your Name <you@example.com>
2023-02-17 17:41:20 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - main function entry point
+* 
+* Return: int(0)
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar((char) (number + 48));
+		putchar(',');
+		if (number == 9)
+			continue;
+		putchar(' ');
+	}
+	return (0);
+}
